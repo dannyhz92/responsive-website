ef65c02f25b15e5d647d7f3e71d166aac6e17712
dannyhz92 <d.hernandez@lafilm.edu>
2018-09-02 15:23:00 -0700

    worked on new section. Added carousel effect

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,15 @@ $(function() {
   // nimate on scroll
   new WOW().init();
 });
+
+
+$(function() {
+
+  $("#work").magnificPopup({
+    delegate:"a",
+    type: "image",
+    gallery: {
+      enabled: true
+    }
+  });
+});
