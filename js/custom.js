2f6ba24ac42590a260a166346ec44030fcab03ca
dannyhz92 <d.hernandez@lafilm.edu>
2018-09-03 15:00:49 -0700

    added team section with new carousel

--- js/custom.js
+++ js/custom.js
@@ -16,3 +16,15 @@ $(function() {
     }
   });
 });
+
+// team section
+
+$(function(){
+  $("#team-members").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true, 
+  });
+});
