8ae18d99e4e0688493696fed7715d89b19eb2eae
dannyhz92 <d.hernandez@lafilm.edu>
2018-09-04 19:41:22 -0700

    Added new testimonial section

--- js/custom.js
+++ js/custom.js
@@ -25,6 +25,16 @@ $(function(){
     autoplay: true,
     smartSpeed: 700,
     loop: true,
-    autoplayHoverPause: true, 
+    autoplayHoverPause: true,
+  });
+});
+
+$(function(){
+  $("#customers-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
   });
 });
