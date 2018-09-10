9b9b4fea8c32a151f21c02c82b8a426ff0eb7492
dannyhz92 <d.hernandez@lafilm.edu>
2018-09-09 20:03:24 -0700

    added a contact section and a clients section

--- js/custom.js
+++ js/custom.js
@@ -46,3 +46,15 @@ $(function(){
   });
 
 });
+
+// clients
+
+$(function(){
+  $("#clients-list").owlCarousel({
+    items: 6,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true,
+  });
+});
